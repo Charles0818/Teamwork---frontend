abef59516ff7c17007e299d68d5becc1ce4fd7e4
Charles <charles081889@gmail.com>
2019-11-28 07:39:18 +0100

    Updates

---
+++ precache-manifest.f477d4241102d55ffee015279c88e4ac.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ba68267169d88a40ae90e403dbd91cf",
+    "url": "/teamwork---frontend/index.html"
+  },
+  {
+    "revision": "23ee1e79dc6b7c4891f1",
+    "url": "/teamwork---frontend/static/css/main.38fa8df0.chunk.css"
+  },
+  {
+    "revision": "9f1bdbef99bbdca0f5d0",
+    "url": "/teamwork---frontend/static/js/2.e8a0a619.chunk.js"
+  },
+  {
+    "revision": "23ee1e79dc6b7c4891f1",
+    "url": "/teamwork---frontend/static/js/main.5bb7323f.chunk.js"
+  },
+  {
+    "revision": "755562cce27374a76e2c",
+    "url": "/teamwork---frontend/static/js/runtime-main.119cd2d4.js"
+  },
+  {
+    "revision": "35fc0890ffb949f03322b85f1bf86ace",
+    "url": "/teamwork---frontend/static/media/design-desk-eyewear-313690.35fc0890.jpg"
+  },
+  {
+    "revision": "ef7ba97b9036a0155f0a0413485edec5",
+    "url": "/teamwork---frontend/static/media/dog.ef7ba97b.gif"
+  },
+  {
+    "revision": "8baeb72db77c03ebe67293da419af6f5",
+    "url": "/teamwork---frontend/static/media/meme.8baeb72d.gif"
+  }
+]);
