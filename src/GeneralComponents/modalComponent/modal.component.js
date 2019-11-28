3bff27e1ecb3ffd01dc8f54a2976f9c16ebf8d5d
Charles <charles081889@gmail.com>
2019-11-28 07:56:23 +0100

    still working on toggleModal function

--- src/GeneralComponents/modalComponent/modal.component.js
+++ src/GeneralComponents/modalComponent/modal.component.js
@@ -1,69 +1,68 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import { createPortal } from "react-dom";
 import './modal.style.css';
 
 const modalRoot = document.querySelector('#modal');
-const root = document.querySelector('#root');
 
-class ModalContent extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { 
-            isModalOpen: this.props.isModalOpen
-         }
-    }
-    render() {
-        (this.props.isModalOpen) ? (
-            root.classList.add('modalRoot')) : (
-                root.classList.remove('modalRoot')
-            )
-        return (
-            <div className={`modal--box ${this.props.isModalOpen ? "active" : ""}`}>
-                <div className="d-flex justify-content--s-between position-relative">
-                    <div className="modal-content--btn">
-                    {this.props.contentBtn ? this.props.contentBtn : null}
-                    </div>
-                    <i className="fas fa-times close font-md" onClick={ (event)=> this.props.toggleModal(event)}></i>
-                </div>
-                <div className="content-wrapper">
-                    <h4 className="modal--heading">{this.props.contentTitle}</h4>
-                        {this.props.contentBody}
-                </div>
+const ModalContent = (props) => {
+  const { isModalOpen } = props;
+  // const [state, setState] = useState(isModalOpen);
+  // useEffect(() => {
+  //   window.addEventListener('click', ()=> props.toggleModal(false))
+  // })
+  // constructor(props) {
+  //   super(props);
+  //   this.state = { 
+  //     isModalOpen: this.props.isModalOpen
+  //   }
+  // }
+
+  // componentDidMount() {
+  //   window.addEventListener('click', ()=> this.props.toggleModal(false))
+  // }
+  // componentWillUnmount() {
+  //   window.removeEventListener('click', ()=> this.props.toggleModal(false))
+  // }
+
+    return (
+      <div className={isModalOpen ? 'modalRoot' : ''}>
+        <div className={`modal--box ${isModalOpen ? "active" : ""}`}>
+          <div className="d-flex justify-content--s-between position-relative">
+            <div className="modal-content--btn">
+            {props.contentBtn ? props.contentBtn : null}
             </div>
-         );
-    }
-}
+            <i className="fas fa-times close font-md" onClick={(event) => props.toggleModal(event)}></i>
+          </div>
+          <div className="content-wrapper">
+            <h4 className="modal--heading">{props.contentTitle}</h4>
+                {props.contentBody}
+          </div>
+        </div>
+      </div>
+    );
+  }
 
 class Modal extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { 
-            isModalOpen: !this.props.isModalOpen
-         }
-         this.modalDiv = document.createElement('div')
-         this.modalDiv.classList.add('modal--wrapper')
-    }
+  constructor(props) {
+    super(props);
+    this.state = {  };
+    this.modalDiv = document.createElement('div');
+    this.modalDiv.classList.add('modal--wrapper');
+  }
 
-    componentDidMount(){
-        modalRoot.appendChild(this.modalDiv);
-    }
+  componentDidMount(){
+    modalRoot.appendChild(this.modalDiv);
+  }
 
-    componentWillUnmount(){
-        modalRoot.removeChild(this.modalDiv);
-    }
+  componentWillUnmount(){
+    modalRoot.removeChild(this.modalDiv);
+  }
 
-    render() {
-        const {isModalOpen} = this.state
-        const ex = ()=> {
-            console.log(isModalOpen)
-        };
-
-        ex()
-        
-        return (
-            createPortal(<ModalContent {...this.props} />, this.modalDiv)
-         );
-    }
+  render() {
+    return (
+      createPortal(<ModalContent {...this.props} />, this.modalDiv)
+    );
+  }
 }
- 
+
 export default Modal;
--- src/GeneralComponents/modalComponent/toggleModal.js
+++ src/GeneralComponents/modalComponent/toggleModal.js
@@ -1,27 +1,38 @@
-// import { useState } from 'react'
-// const toggleModal = ()=> {
-// const [state, setState] = useState({isModalOpen: false})
-// const handleAction = (event) => {
-//     if (event) {
-//         event.preventDefault();
-//         const target = event.target;
-//         console.log(target);
-//         const targetClassList = [...target.classList];
-//         console.log(targetClassList);
-//         (targetClassList.includes('close')) ? (
-//         setState(state => (state.isModalOpen = false)) 
-//         ) : (
-//             setState(state => state.isModalOpen = true)
-//         )
-//     }
-// }
-//     const { isModalOpen } = state;
-//     return {
-//         isModalOpen,
-//         handleAction
-//     }
-// }
+import React, { useState, useEffect } from 'react'
+import Modal from './modal.component';
+const useModal = (bool)=> {
+    const [state, setState] = useState({isModalOpen: null});
+    useEffect(() => {
+        setState({isModalOpen: bool});
+    }, [bool])
+    const { isModalOpen } = state;
+    return isModalOpen
+}
+const useModalState = (bool) => useModal(bool)
 
-// export default toggleModal
+const ToggleModal = (event, Content, title) => {
+    event.preventDefault();
 
-// //try making this a class component
+    let bool = null;
+    const target = event.target;
+    console.log(target);
+    const targetClassList = [...target.classList];
+    console.log(targetClassList);
+    targetClassList.includes('close') ? bool = false : bool = true;
+    const isModalOpen = useModalState(bool)
+     return (
+        <Modal toggleModal = {'hi'} isModalOpen = {isModalOpen} contentTitle ={title} contentBody = {<Content />} />
+     )
+}
+// const useModalState = (event) => {
+//     event.preventDefault();
+//     const target = event.target;
+//     console.log(target);
+//     const targetClassList = [...target.classList];
+//     console.log(targetClassList);
+   
+//     return (
+//         <Modal useModalState = {this.useModalState} isModalOpen = {state} contentTitle ="View Comments" contentBody = {<Comments />} />
+//     )
+// }
+export default ToggleModal
