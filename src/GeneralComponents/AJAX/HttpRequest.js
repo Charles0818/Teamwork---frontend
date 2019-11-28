546bf5c43de7713271e35496ac043ba25e6873a4
Charles <charles081889@gmail.com>
2019-11-28 07:53:52 +0100

    modified ajax fetch function

--- src/GeneralComponents/AJAX/HttpRequest.js
+++ src/GeneralComponents/AJAX/HttpRequest.js
@@ -1,11 +1,30 @@
 const sendHttpRequest = (method, url, data, authToken ) => {
+    if(method === 'GET' || method === 'DELETE') {
+        return fetch(url, {
+            method:method,
+            mode: 'cors',
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Authorization': authToken ? `Bearer ${authToken}` : ""
+            }
+        }).then(res => {
+            if(res.status >= 400) {
+                return res.json().then(errRes => {
+                    throw errRes
+                });
+            }
+            return res.json()
+        });
+    }
     return fetch(url, {
         method:method,
-        body:JSON.stringify(data),
-        headers: data ? {
+        mode: 'cors',
+        body: JSON.stringify(data),
+        headers: {
+            'Access-Control-Allow-Origin': '*',
             'Content-Type': 'application/json',
-            'authorization': authToken ? `Bearer ${authToken}` : ""
-        } : {'Authorization': `Bearer ${authToken}`}
+            'Authorization': authToken ? `Bearer ${authToken}` : ""
+        }
     }).then(res => {
         if(res.status >= 400) {
             return res.json().then(errRes => {
@@ -18,6 +37,8 @@ const sendHttpRequest = (method, url, data, authToken ) => {
 
 exports.apiKey = 'https://teamworkbycharles.herokuapp.com/api/v1';
 
-exports.getData = (url) => sendHttpRequest('GET', url).then(data => console.log(data))
+exports.getData = (url, userId, authToken) => sendHttpRequest('GET', `${url}/${userId}`, null, authToken)
 
-exports.sendData = (url, data) => sendHttpRequest('POST', url, data)
+exports.sendData = (url, data, authToken) => sendHttpRequest('POST', url, data, authToken);
+exports.modifyData = (url, data, authToken) => sendHttpRequest('PATCH', url, data, authToken);
+exports.deleteData = (url, userId, authToken) => sendHttpRequest('DELETE', `${url}/${userId}`, null, authToken);
