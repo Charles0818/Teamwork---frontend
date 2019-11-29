744c8cbb107478ebb0540cd2015e237b1c7363d0
Charles <charles081889@gmail.com>
2019-11-29 02:59:03 +0100

    Updates

--- precache-manifest.d19af54e776344f04d3913891e1989f9.js
+++ precache-manifest.d19af54e776344f04d3913891e1989f9.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2cd21a8725f0b9885d4803f7b96bcf65",
+    "revision": "75d9e601a3fb56fe9476917e5d70db73",
     "url": "/Teamwork---frontend/index.html"
   },
   {
-    "revision": "404568f333774c321199",
+    "revision": "44494e7eb8054d53ddfc",
     "url": "/Teamwork---frontend/static/css/main.7b8ed502.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Teamwork---frontend/static/js/2.b7c48db5.chunk.js"
   },
   {
-    "revision": "404568f333774c321199",
-    "url": "/Teamwork---frontend/static/js/main.4eedda72.chunk.js"
+    "revision": "44494e7eb8054d53ddfc",
+    "url": "/Teamwork---frontend/static/js/main.629688e1.chunk.js"
   },
   {
     "revision": "1b20360a30ec1057036d",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Teamwork---frontend/precache-manifest.4ae7d354db87496f2b12c7fa663fb5c5.js"
+  "/Teamwork---frontend/precache-manifest.d19af54e776344f04d3913891e1989f9.js"
 );
 
 self.addEventListener('message', (event) => {
