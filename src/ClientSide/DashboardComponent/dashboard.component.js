674092369670c8ec3cb3b077a51f8b85a0faaf60
Charles <charles081889@gmail.com>
2020-01-07 22:36:14 +0100

    Client dashboard component

--- src/ClientSide/DashboardComponent/dashboard.component.js
+++ src/ClientSide/DashboardComponent/dashboard.component.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React, { useEffect, useContext } from 'react';
 import '../Sidebars/sidebar.style.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import LeftSidebar from '../Sidebars/LeftSidebar.component';
 import RightSidebar from '../Sidebars/RightSidebar.component';
 import MainContent from '../MainContent/MainContent.component';
@@ -8,45 +8,31 @@ import { getData, apiKey } from '../../GeneralComponents/AJAX/HttpRequest';
 import { addDefaultAvatar } from '../../GeneralComponents/context/addDefaultAvatar';
 import { UsersContext } from '../../GeneralComponents/context/usersContext';
 
+const Dashboard = (props) =>  {
+ const {  users, addUser  } = useContext(UsersContext);
+ const { match: { path }, updateAccount, userData: { data:  { userId, token, photoDetails, interests } } } = props;
+ useEffect(() => {
+  if(users.length <= 0) {
+    getData(`${apiKey}/auth/users`, userId, token)
+    .then(res => {
+      let { data } = res;
+      data = addDefaultAvatar(data)
+      addUser(data);
+    }).catch(err => {
+      console.log(err);
+    })
+  }else return
+ })
 
-class Dashboard extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { 
-      
-     }
-  }
-
-  static contextType = UsersContext;
-
-  UNSAFE_componentWillMount() {
-    const { users, addUser } = this.context;
-    if(users.length <= 0) {
-      const { data:  { userId, token } } = this.props.userData;
-      getData(`${apiKey}/auth/users`, userId, token)
-      .then(res => {
-        let { status, data } = res;
-        data = addDefaultAvatar(data)
-        addUser(data)
-      }).catch(err => {
-        console.log(err);
-      })
-    }else return
-  }
-
-  render() {
-    const { updateAccount, userData } = this.props;
-    const { data: {photoDetails, interests, userId} } = userData;
-    return (
-      <Router>
-        <section className="page-section d-flex">
-          <LeftSidebar userData = {{ photoDetails, interests }} updateAccount={updateAccount} />}
-          <Route path={this.props.path} component={ MainContent } />
-          <RightSidebar userId={userId} />
-        </section>
-      </Router>
-    );
-  }
+  return (
+    <Router>
+      <section className="page-section d-flex">
+        <LeftSidebar path={path} userData = {{ photoDetails, interests }} updateAccount={updateAccount} />
+        <Route path={path} component={ MainContent } />
+        <RightSidebar userId={userId} />
+      </section>
+    </Router>
+  );
 }
 
 export default Dashboard;
