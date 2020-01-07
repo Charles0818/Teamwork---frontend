dfaa4e6c99a26837f7dc4c0be27808d305dcd164
Charles <charles081889@gmail.com>
2020-01-07 22:31:49 +0100

    set up employees list component

--- src/Admin/ListEmployees/ListEmployees.component.js
+++ src/Admin/ListEmployees/ListEmployees.component.js
@@ -1,37 +1,25 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
-import { apiKey, getData } from '../../GeneralComponents/AJAX/HttpRequest';
+import { Link } from 'react-router-dom';
 import { UsersContext } from '../../GeneralComponents/context/usersContext';
+import { widthIsMatch } from '../../GeneralComponents/HelperFunctions/mediaQueryCheck';
 import '../admin.style.css';
 
 class ListEmployees extends Component {
   constructor(props) {
       super(props);
+      this._isMounted = false;
       this.state = {  }
   }
 
   static contextType = UsersContext;
 
-  componentWillMount() {
-    const { users, addUser } = this.context;
-    if(users.length <= 0) {
-      const { userId, token } = this.props.userData;
-      getData(`${apiKey}/auth/users`, userId, token)
-      .then(res => {
-        const { status, data } = res;
-        addUser(data)
-      }).catch(err => {
-        console.log(err);
-      })
-    }else return
-  }
   render() {
     const { users, addUser } = this.context;
     console.log(this.props.match);
     return (
-      <section className="d-flex d-flex justify-content--center align-items--center" style={{width: '100%', height: '80vh'}}>
-        <div className="d-flex justify-content--center align-items--center padding-lg">
-          {users.map((user, index) => <Employee key={index} user={user} />)}
+      <section className="d-flex justify-content--center align-items--center" style={{width: '100%', height: '80vh'}}>
+        <div className={`d-flex justify-content--center align-items--center ${widthIsMatch('991.98px') ? 'padding-md' : 'padding-lg'}`}>
+          {users.map((user, index) => <Employee key={index} user={user} addUser={addUser} />)}
         </div>
       </section>
     );
@@ -45,24 +33,22 @@ class Employee extends Component {
   }
 
   render() {
-    const { photoDetails, EmployeeId, firstName, lastName, } = this.props.user;
+    const { photoDetails, employeeId, firstName, lastName, } = this.props.user;
     return (
       <div className="link-wrap--box">
-        <div className="employee--box bckgr-white cursor-pointer">
-          <div className="user-avatar margin-bottom-sm position-relative">
+        <div className="employee--box cursor-pointer border-r-10 overflow--h">
+          <div className="user-avatar position-relative">
             <img src={photoDetails[0]} alt="user avatar" />
-            <div class="action-btns">
-              <i class="fas fa-trash margin-right-sm font-md border-r-circle danger-text"
-              onClick={this.confirmAction()}></i>
+            <div class="action-btns d-flex nowrap">
+              <i class="fas fa-trash margin-right-sm font-md border-r-circle danger-text"></i>
               <Link to={`/user/${firstName}_${lastName}`}>
-                <i class="fas fa-eye margin-right-sm font-md border-r-circle primary-color"></i>
+                <i class="fas fa-eye margin-right-sm font-md border-r-circle color-white"></i>
               </Link>
             </div>
           </div>
-          {/* <div className="text-align-center color-dark"> */}
-            {/* <i className="fas fa-user-plus icon margin-bottom-sm font-lg color2"></i>  */}
+          <div className="padding-sm color-dark bckgr-white">
             <div className="font-sm padding-sm font-weight-bold color-dark">{`${firstName} ${lastName}`}</div>
-          {/* </div> */}
+          </div>
         </div>
       </div>
     )
