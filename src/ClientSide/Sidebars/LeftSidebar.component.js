6b31d885f16de7281e46976ca568f53de43c1643
Charles <charles081889@gmail.com>
2020-01-07 22:59:53 +0100

    rewritten to function components

--- src/ClientSide/Sidebars/LeftSidebar.component.js
+++ src/ClientSide/Sidebars/LeftSidebar.component.js
@@ -1,66 +1,56 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
+import { slideLeftSidebar } from '../../GeneralComponents/HelperFunctions/sidebar';
 import './sidebar.style.css';
-class LeftSidebar extends Component {
-  constructor(props) {
-      super(props);
-      this.state = {  }
-  }
-
-  handleLogout = () => {
-    const isLoggedIn = false;
-    const { updateAccount, userData } = this.props;
-    const data = {
-      isLoggedIn,
-      userData
-    }
+const LeftSidebar = (props) => {
+  const { updateAccount, userData, userData: { photoDetails, interests, accountType } } = props;
+  const handleLogout = () => {
+    const data = { isLoggedIn: false, userData }
     updateAccount(data);
-
   }
-  render() {
-    const { userData: { photoDetails, interests, accountType } } = this.props;
-    console.log(this.props.userAccount)
-    return ( 
-      <aside>
-        <div className="aside left">
-          <div className="padding-top-lg">
-            <div className="about-user border-line--h d-flex justify-content--center column align-items--center padding-bottom-sm">
-              <div className="user_avatar avatar--lg">
-                <img src={photoDetails[0]} alt="user avatar" />
-              </div>
-              <div className="interests margin-top-md d-flex padding-md">
-                { interests !== null ? (
-                  interests.map((interest, index) => {
-                    return <span key={index} className="margin-right-sm margin-bottom-sm btn font-sm padding-sm">{interest}</span>
-                  })
-                ) : null}
-              </div>
-            </div>
-            <div className="content-wrapper">
-              <ul className="margin-bottom-md">
-                {accountType === "admin" ? (
-                  <Link to="/admin/dashboard">
-                    <li className="sidebar-link padding-bottom-md margin-bottom-sm cursor-pointer">
-                      <i className="fas fa-cogs color-white padding-right-sm font-sm"></i>
-                      <span className="sidebar--link-text font-sm">Admin Panel</span>
-                    </li>
-                </Link>
-                ) : null}
-                <li className="sidebar-link padding-bottom-md margin-bottom-sm cursor-pointer">
-                  <i className="fas fa-newspaper color-white padding-right-sm font-md"></i>
-                  <span className="sidebar--link-text font-md">Dashboard</span>
-                </li>
-                <li className="sidebar-link padding-bottom-md margin-bottom-sm cursor-pointer cursor-pointer" onClick={() => this.handleLogout()}>
-                  <i className="fas fa-sign-out-alt color-white padding-right-sm font-sm"></i>
-                  <span className="sidebar--link-text font-md">Log out</span>
-                </li>
-              </ul>
-            </div>
+  return ( 
+    <aside className="aside left padding-top-sm">
+      <div className="position-relative">
+        <i className="fas fa-times close color-white font-sm cursor-pointer"
+          onClick={(event) => slideLeftSidebar(event)}>
+        </i>
+      </div>
+      <div className="padding-top-md">
+        <div className="about-user border-line--h d-flex justify-content--center column align-items--center padding-bottom-sm">
+          <Link to='/user/profile' className="user_avatar avatar--lg">
+            <img src={photoDetails[0]} alt="user avatar" />
+          </Link>
+          <div className="interests margin-top-md d-flex padding-md">
+            { interests !== null ? (
+              interests.map((interest, index) => {
+                return <span key={index} className="margin-right-sm margin-bottom-sm btn font-sm padding-sm">{interest}</span>
+              })
+            ) : null}
           </div>
         </div>
-      </aside>
-    );
-  }
+        <div className="content-wrapper">
+          <ul className="margin-bottom-md">
+            {accountType === "admin" ? (
+              <Link to="/admin/dashboard">
+                <li className="sidebar-link padding-bottom-md margin-bottom-sm cursor-pointer">
+                  <i className="fas fa-cogs color-white padding-right-sm font-sm"></i>
+                  <span className="sidebar--link-text font-sm">Admin Panel</span>
+                </li>
+            </Link>
+            ) : null}
+            <li className="sidebar-link padding-bottom-md margin-bottom-sm cursor-pointer">
+              <i className="fas fa-newspaper color-white padding-right-sm font-md"></i>
+              <span className="sidebar--link-text font-md">Dashboard</span>
+            </li>
+            <li className="sidebar-link padding-bottom-md margin-bottom-sm cursor-pointer cursor-pointer" onClick={() => handleLogout()}>
+              <i className="fas fa-sign-out-alt color-white padding-right-sm font-sm"></i>
+              <span className="sidebar--link-text font-md">Log out</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </aside>
+  );
 }
 
 export default LeftSidebar;
--- src/ClientSide/Sidebars/RightSidebar.component.js
+++ src/ClientSide/Sidebars/RightSidebar.component.js
@@ -1,56 +1,49 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { UsersContext } from '../../GeneralComponents/context/usersContext';
+import { slideRightSidebar } from '../../GeneralComponents/HelperFunctions/sidebar';
 import './sidebar.style.css';
-class RightSidebar extends Component {
-  constructor(props) {
-      super(props);
-      this.state = {  }
-  }
 
-  static contextType = UsersContext;
-  render() {
-    let { users } = this.context;
-    console.log(users);
-    users = users.map((user, index) => {
-      const { photoDetails, firstName, lastName, employeeId } = user;
-      return { photoDetails, firstName, lastName, employeeId }
-     });
-     const userId = this.props.userId;
-    console.log(users);
-    return ( 
-      <aside className="thread">
-          <div className="aside right">
-            <div className="padding-top-lg">
-              <div className="accordion">
-                <div className="accordion--btn d-flex justify-content--center">
-                  <span className="font-md font-weight-600">All Colleagues</span>
-                </div>
-                <div className="content-wrapper">
-                  <ul className="online--users">
-                    {users.map((user, index) => {
-                      const { photoDetails, firstName, lastName, employeeId } = user
-                      return (
-                        <Link to={`${userId === employeeId ? '/user/profile' : `/users/${firstName}_${lastName}`}`} key={index}>
-                          <li className="user d-flex align-items--center margin-bottom-md nowrap action" key={index}>
-                            <div className="position-relative">
-                              <img className="avatar--sm icon" src={photoDetails[0]} alt="poster avatar" />
-                              <span className="online"></span>
-                            </div>
-                            <span className="font-sm username">{`${firstName} ${lastName} ${userId === employeeId ? '(You)' : ''}`}</span>
-                          </li>
-                        </Link>
-                      )
-                    })}
-                    
-                  </ul>
-                </div>
-            </div>
+const RightSidebar = (props) => {
+  const { userId } = props;
+  let { users } = useContext(UsersContext);
+  users = users.map((user) => {
+    const { photoDetails, firstName, lastName, employeeId } = user;
+    return { photoDetails, firstName, lastName, employeeId }
+  });
+  
+  return ( 
+    <aside className="aside right bckgr-white padding-top-md">
+      <div className="position-relative">
+        <i className="fas fa-times close font-sm cursor-pointer"
+          onClick={(event) => slideRightSidebar(event)}>
+        </i>
+        <div className="accordion">
+          <div className="accordion--btn d-flex justify-content--center">
+            <span className="font-md font-weight-600">All Colleagues</span>
+          </div>
+          <div className="content-wrapper">
+            <ul className="online--users">
+              {users.map((user, index) => {
+                const { photoDetails, firstName, lastName, employeeId } = user
+                return (
+                  <Link to={`${userId === employeeId ? '/user/profile' : `/user/${firstName}_${lastName}`}`} key={index}>
+                    <li className="user d-flex align-items--center margin-bottom-md nowrap action" key={index}>
+                      <div className="position-relative">
+                        <img className="avatar--sm icon" src={photoDetails[0]} alt="poster avatar" />
+                        <span className="online"></span>
+                      </div>
+                      <span className="font-sm username">{`${firstName} ${lastName} ${userId === employeeId ? '(You)' : ''}`}</span>
+                    </li>
+                  </Link>
+                )
+              })}
+            </ul>
           </div>
         </div>
-      </aside>
-    );
-  }
+      </div>
+    </aside>
+  );
 }
 
 export default RightSidebar;
