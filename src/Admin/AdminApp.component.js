49c09ef5009b570c80985235f555d41a439f1939
Charles <charles081889@gmail.com>
2020-01-07 22:34:06 +0100

    set up Admin App

--- src/Admin/AdminApp.component.js
+++ src/Admin/AdminApp.component.js
@@ -1,34 +1,57 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch, withRouter } from 'react-router-dom';
 
 import Navbar from '../GeneralComponents/Navbar/Navbar.component';
 import AdminDashboard from './Dashboard/dashboard';
 import ListEmployees from './ListEmployees/ListEmployees.component';
-// import Profile from '../ClientSide/Profile/profile.component';
-// import Dashboard from '../ClientSide/DashboardComponent/dashboard.component';
-import UserApp from '../ClientSide/UserApp.component';
+// import UserApp from '../ClientSide/UserApp.component';
 import { UserContext } from '../GeneralComponents/context/userContext';
+import { getData, apiKey } from '../GeneralComponents/AJAX/HttpRequest';
+import { addDefaultAvatar } from '../GeneralComponents/context/addDefaultAvatar';
+
 class AdminApp extends Component {
   constructor(props) {
     super(props);
     this.state = {  }
   }
   static contextType = UserContext;
+
+  componentWillMount() {
+    this._isMounted = true;
+    const { users, addUser, history: { push }, match: { path } } = this.props;
+    console.log(this.props);
+    if(users.length === 0) {
+      const { data:  { userId, token } } = this.context;
+      getData(`${apiKey}/auth/users`, userId, token)
+      .then(res => {
+        let { data } = res;
+        data = addDefaultAvatar(data);
+        addUser(data);
+        push(`${path}/dashboard`)
+      }).catch(err => {
+        console.log(err);
+      })
+    }else return 
+  }
+
+  componentWillUnmount() {
+    this._isMounted = false;
+  }
   render() {
     const { data: { token, userId } } = this.context;
-    const { match } = this.props;
+    const { match, users, addUser } = this.props;
     return (
       <Router>
         <Route path={`${match.path}`} component={Navbar} />
           <Switch>
             <Route exact path={`${match.path}/dashboard`} component={AdminDashboard} />
-            <Route exact path={`${match.path}/dashboard/employees`}
-              render={(props) => <ListEmployees userData={{token, userId}} {...props} /> } />
-            <Route path="/user" component={UserApp} />
+            <Route exact path={`${match.path}/employees`}
+              render={(props) => <ListEmployees users={users} addUser={addUser} userData={{token, userId}} {...props} /> } />
+            {/* <Route path="/user" component={UserApp} /> */}
           </Switch>
       </Router>
     );
   }
 }
 
-export default AdminApp;
+export default withRouter(AdminApp);
