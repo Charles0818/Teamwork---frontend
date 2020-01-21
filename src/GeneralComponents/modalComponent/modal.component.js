174f99a7b0e8a938f54a306ce1ac872ee63a9784
Charles <charles081889@gmail.com>
2020-01-21 22:00:21 +0100

    deleted commented lines

--- src/GeneralComponents/modalComponent/modal.component.js
+++ src/GeneralComponents/modalComponent/modal.component.js
@@ -1,46 +1,31 @@
-import React, { Component, useState, useEffect } from 'react';
+import React, { Component, useEffect } from 'react';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { createPortal } from "react-dom";
 import './modal.style.css';
 
 const modalRoot = document.querySelector('#modal');
 
 const ModalContent = (props) => {
-  const { isModalOpen } = props;
-  // const [state, setState] = useState(isModalOpen);
-  // useEffect(() => {
-  //   window.addEventListener('click', ()=> props.toggleModal(false))
-  // })
-  // constructor(props) {
-  //   super(props);
-  //   this.state = { 
-  //     isModalOpen: this.props.isModalOpen
-  //   }
-  // }
-
-  // componentDidMount() {
-  //   window.addEventListener('click', ()=> this.props.toggleModal(false))
-  // }
-  // componentWillUnmount() {
-  //   window.removeEventListener('click', ()=> this.props.toggleModal(false))
-  // }
-
-    return (
-      <div className={isModalOpen ? 'modalRoot' : ''}>
-        <div className={`modal--box ${isModalOpen ? "active" : ""}`}>
-          <div className="d-flex justify-content--s-between position-relative">
-            <div className="modal-content--btn">
-            {props.contentBtn ? props.contentBtn : null}
-            </div>
-            <i className="fas fa-times close font-md" onClick={(event) => props.toggleModal(event)}></i>
+  const { isModalOpen, closeModal } = props;
+ 
+  return (
+    <div className={isModalOpen ? 'modalRoot' : ''}>
+      <div className={`modal--box ${isModalOpen ? "active" : ""}`}>
+        <div className="d-flex justify-content--s-between position-relative">
+          <div className="modal-content--btn">
+          {props.contentBtn ? props.contentBtn : null}
           </div>
-          <div className="content-wrapper">
-            <h4 className="modal--heading">{props.contentTitle}</h4>
-                {props.contentBody}
-          </div>
+          <i className="fas fa-times close font-md" onClick={closeModal}></i>
+          {/* <FontAwesomeIcon icon="times" className="close font-md" onClick={closeModal} /> */}
+        </div>
+        <div className="content-wrapper">
+          <h4 className="modal--heading">{props.contentTitle}</h4>
+            {props.contentBody}
         </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
 
 class Modal extends Component {
   constructor(props) {
