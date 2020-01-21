4ccddc4a96e10539dff1300c5ecc1bdd467a88cc
Charles <charles081889@gmail.com>
2020-01-21 21:49:33 +0100

    Removed useEffect

--- src/ClientSide/DashboardComponent/dashboard.component.js
+++ src/ClientSide/DashboardComponent/dashboard.component.js
@@ -1,29 +1,11 @@
-import React, { useEffect, useContext } from 'react';
+import React from 'react';
 import '../Sidebars/sidebar.style.css';
 import { HashRouter as Router, Route } from 'react-router-dom';
 import LeftSidebar from '../Sidebars/LeftSidebar.component';
 import RightSidebar from '../Sidebars/RightSidebar.component';
 import MainContent from '../MainContent/MainContent.component';
-import { getData, apiKey } from '../../GeneralComponents/AJAX/HttpRequest';
-import { addDefaultAvatar } from '../../GeneralComponents/context/addDefaultAvatar';
-import { UsersContext } from '../../GeneralComponents/context/usersContext';
-
 const Dashboard = (props) =>  {
- const {  users, addUser  } = useContext(UsersContext);
- const { match: { path }, updateAccount, userData: { data:  { userId, token, photoDetails, interests } } } = props;
- useEffect(() => {
-  if(users.length <= 0) {
-    getData(`${apiKey}/auth/users`, userId, token)
-    .then(res => {
-      let { data } = res;
-      data = addDefaultAvatar(data)
-      addUser(data);
-    }).catch(err => {
-      console.log(err);
-    })
-  }else return
- })
-
+ const { match: { path }, updateAccount, userData: { data:  { userId, photoDetails, interests } } } = props;
   return (
     <Router>
       <section className="page-section d-flex">
