3dc23019d9f1e1ef33e60395fe0fa185a2307719
Charles <charles081889@gmail.com>
2020-01-21 22:06:28 +0100

    added ChatApp route

--- src/App.js
+++ src/App.js
@@ -1,29 +1,27 @@
 import React, { Component } from 'react';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {HashRouter as Router, Route, Switch} from 'react-router-dom';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import { far } from '@fortawesome/free-regular-svg-icons';
-import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
 import './App.css';
 
 //Components
 import LandingPage from './Landing_page/LandingPage.component';
-// import Navbar from './GeneralComponents/Navbar/Navbar.component';
+import ChatApp from './ClientSide/ChatApp/index';
 import UserApp from './ClientSide/UserApp.component';
 import AdminApp from './Admin/AdminApp.component';
 import ProtectedRoute from './GeneralComponents/PrivateRoute/ProtectedRoute.component';
 
-//User Context
+//Context
 import UserContextProvider from './GeneralComponents/context/userContext';
 import UsersContextProvider from './GeneralComponents/context/usersContext';
 import  { IsAuth, IsAdmin } from './GeneralComponents/context/auth.component';
 
-library.add(far, fab, faCheckSquare, faCoffee);
+library.add(far, fab, fas);
 
 class App extends Component {
   render () {
-    // const IAdmin = Auth()
-    // console.log(IAdmin)
     return (
       <Router>
         <UserContextProvider>
@@ -32,6 +30,7 @@ class App extends Component {
               <Switch>
                 <ProtectedRoute path="/user" auth={IsAuth} component={UserApp} />
                 <ProtectedRoute path="/admin" auth={IsAdmin} component={AdminApp} />
+                <ProtectedRoute path="/chats" auth={IsAuth} component={ChatApp} />
               </Switch>
           </UsersContextProvider>
         </UserContextProvider>
