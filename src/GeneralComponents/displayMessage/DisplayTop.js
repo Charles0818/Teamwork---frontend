195b46dbaf2750cb5a484ef6f5e0d6314ba49945
Charles <charles081889@gmail.com>
2020-01-21 21:59:12 +0100

    Display message prior to certain conditions

--- src/GeneralComponents/displayMessage/DisplayTop.js
+++ src/GeneralComponents/displayMessage/DisplayTop.js
@@ -1,17 +1,53 @@
-import React, { Component } from 'react';
-export class DisplayMessageTop extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {  }
-    }
-    render() { 
-        return ( 
-            <div className="d-flex justify-content--center padding-md border-r-10 bckgr-white color-success" style={{position: 'fixed', top: '0'}}>
-                <div className="d-flex align-self-end">
-                    <i className="fas fa-good font-lg margin-right-md"></i>
-                    <span className="font-lg">{this.props.message}</span>
-                </div>
-            </div>
-         );
-    }
+import React, { useState, useEffect } from 'react';
+import { createPortal } from 'react-dom';
+import './message.style.css';
+const modalRoot = document.querySelector('#modal');
+
+export const Message = (props) => {
+ const [effect, setEffect] = useState(false);
+ useEffect(() => setEffect(true),[])
+  const { status } = props;
+  return (
+    <div className={`d-flex justify-content--center httpStatus
+      ${effect ? 'active' : ''} padding-lg border-r-10 bckgr-white
+      ${status === 'success' ? 'success-text' : 'danger-text'}`}>
+      <div className={`d-flex align-self-end ${status === 'success' ? 'success-text' : 'danger-text'}`}>
+        <i className={`fas fa-check font-lg margin-right-md ${status === 'success' ? 'success-text' : 'danger-text'}`}></i>
+        <span className={`font-lg ${status === 'success' ? 'success-text' : 'danger-text'}`}>{props.message}</span>
+      </div>
+    </div>
+  );
+}
+
+const DisplayHttpStatus = (props) => {
+  useEffect(() => {
+    modalRoot.appendChild(modalDiv);
+    const interval = setTimeout(() => {
+      modalRoot.removeChild(modalDiv)
+    }, 3000);
+    return () =>  clearTimeout(interval)
+  })
+    const modalDiv = document.createElement('div');
+    modalDiv.classList.add('modal--wrapper');
+
+    return (
+      createPortal(<Message {...props} />, modalDiv)
+    );
+}
+
+export default DisplayHttpStatus;
+
+export const useHttpStatus = () => {
+  const [ajaxStatus, setAjaxStatus] = useState({show: false, message: '', status: ''});
+  const updateAjaxStatus = (props) => {
+    const { show, message, status } = props;
+    setAjaxStatus({show, message, status});
+    const timeout = setTimeout(() => {
+      setAjaxStatus({show: false, message: '', status: ''});
+    }, 3500);
+    return () => clearTimeout(timeout)
+  };
+  const HttpStatusComponent = ajaxStatus.show &&
+    <DisplayHttpStatus status={ajaxStatus.status} message={ajaxStatus.message} />
+  return { ajaxStatus, updateAjaxStatus, HttpStatusComponent }
 }
